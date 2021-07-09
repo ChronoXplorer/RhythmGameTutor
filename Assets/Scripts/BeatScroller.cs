34840aee356405d67580cdda6baf608cd7308471
ChronoXplorer <57399759+ChronoXplorer@users.noreply.github.com>
2021-07-09 12:47:33 +0700

    Set the note to move match the BPM that has been set to

--- Assets/Scripts/BeatScroller.cs
+++ Assets/Scripts/BeatScroller.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class BeatScroller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float beatTempo;
+
+    public bool songStart;
+
     void Start()
     {
-        
+        beatTempo = beatTempo / 60f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(!songStart)
+        {
+            if(Input.anyKeyDown)
+            {
+                songStart = true;
+            }
+        }
+        else
+        {
+            transform.position += new Vector3(0f, beatTempo * Time.deltaTime, 0f);
+        }
         
     }
 }
